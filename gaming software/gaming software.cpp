d4365808bce2b306263686a415126717d6228922
esqa <37853883+esqa@users.noreply.github.com>
2022-02-25 00:03:36 +0100

    Release
    Release

---
+++ gaming software/gaming software.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int main()
+{
+
+	for (char lwalpCh = 'a'; lwalpCh <= 'z'; lwalpCh++)
+	{
+		cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n"; // 19
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+		cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "", cout << lwalpCh << "\n";
+
+	}
+
+	Sleep(9999999999);
+}
